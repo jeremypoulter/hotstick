6dd6bca634270960f847d8f01995b0aa2059b4f0
Jeremy Poulter <jeremy@bigjungle.net>
2016-10-19 19:20:44 +0100

    Started adding MPU6050 support and removal of MAX thermocouple support

--- Wand/Firmware/Wand_Firmware/src/main.cpp
+++ Wand/Firmware/Wand_Firmware/src/main.cpp
@@ -2,42 +2,60 @@
 //
 // Jeremy Poulter
 
-// Using example code from;
-// MAX6675
-// www.ladyada.net/learn/sensors/thermocouple
+#include <Arduino.h>
 
-#include "max6675.h"
+#include "I2Cdev.h"
 
-#define LED_RED   3
-#define LED_GREEN 5
-#define LED_BLUE  6
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
 
-int thermoDO = MISO;
-int thermoCS = SS;
-int thermoCLK = SCK;
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
 
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
-int vccPin = 3;
-int gndPin = 2;
+#define LED_RED         3
+#define LED_GREEN       5
+#define LED_BLUE        6
 
-void setup() {
-  Serial.begin(9600);
-  // use Arduino pins 
-  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
-  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
+#define HEAT_DETECT_PIN A0
+#define TEMP_PIN        A1
+#define INTERRUPT_PIN   2  // use pin 2 on Arduino Uno & most boards
 
-  Serial.println("MAX6675 test");
-  // wait for MAX chip to stabilize
-  delay(500);
+// MPU control/status vars
+MPU6050 mpu;
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// Temperature related functions
+float readTemp();
+
+// MPU/DMP related stuff
+void mpu_setup();
+void dmpDataReady();
+
+// LED control
+void led_setup();
+void led(int red, int green, int blue);
+
+void setup()
+{
+  // Initialise the serial
+  Serial.begin(38400);
+
+  led_setup();
+  mpu_setup();
 }
 
-void loop() {
-  // basic readout test, just print the current temp
+void loop()
+{
 
-  Serial.print("C = ");
-  Serial.println(thermocouple.readCelsius());
-
-  delay(1000);
 }
 
 void led(int red, int green, int blue)
@@ -45,4 +63,77 @@ void led(int red, int green, int blue)
     analogWrite(LED_RED, 255 - red);
     analogWrite(LED_GREEN, 255 - green);
     analogWrite(LED_BLUE, 255 - blue);
+}
+
+void led_setup()
+{
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+}
+
+float readTemp()
+{
+  return 22.0;
+}
+
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void mpu_setup()
+{
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
 }
